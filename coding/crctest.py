1fb814a33946dba411efd0375d61a02caf5f909a
Damon Du <damon_dulc@163.com>
2020-05-20 20:06:04 +0800

    add a crc test file

---
+++ coding/crctest.py
@@ -0,0 +1,16 @@
+def calc_crc(string):
+    data = bytearray.fromhex(string)
+    crc = 0xFFFF
+    for pos in data:
+        crc ^= pos
+        for i in range(8):
+            if ((crc & 1) != 0):
+                crc >>= 1
+                crc ^= 0xA001
+            else:
+                crc >>= 1
+    return hex(((crc & 0xff) << 8) + (crc >> 8))
+
+
+crc = calc_crc('01020304')
+print(crc)
