b8453d9c977485e9e5ad16d1ebeebc1ae22c8f06
Damon Du <damon_dulc@163.com>
2020-06-13 12:33:17 +0800

    添加了打孔参数

--- coding/conv_encode.py
+++ coding/conv_encode.py
@@ -5,11 +5,11 @@
 import numpy
 from numpy import array
 import commpy.channelcoding.convcode as cc
+import math
 
 
 # 编码函数
-# todo：G2反相；打孔
-def encode(message: bytes, gen: list, m: int, g2reverse = False):
+def encode(message: bytes, gen: list, m: int, g2reverse = False, puncture_matrix = None):
     '''
     卷积编码,返回编码后字节
     :param message:待编码字节
@@ -25,17 +25,37 @@ def encode(message: bytes, gen: list, m: int, g2reverse = False):
     trellis = cc.Trellis(memory, g_matrix)
     # print(trellis.next_state_table)
     # 编码,返回比特数组
+    # 没找到G2反相的参数，先编码，再对G2反相，再打孔
     # 可选的参数termination，对应编码过程中的尾码，选择‘term’输出尾码，选择'cont'咬尾，不输出尾码
     array_bits_convcoded = cc.conv_encode(array_bits_message,
                                           trellis,
                                           termination='term')
     print(array_bits_convcoded, len(array_bits_convcoded))
+    # 如果G2反相，进行处理
     if g2reverse:
         for i in range(int(len(array_bits_convcoded)/2)):
             array_bits_convcoded[2*i+1] -= 1
     array_bits_convcoded = abs(array_bits_convcoded)
     print(array_bits_convcoded, len(array_bits_convcoded))
     
+    # 打孔处理
+    if puncture_matrix is not None:
+        # 构建打孔矩阵
+        try:
+            p = numpy.array(puncture_matrix)
+        except:
+            print('打孔矩阵不正确,请检查')
+        # 组合起来，构建bool列表，实现打孔
+        pm = p.T.flatten()
+        P_m_bool = pm > 0
+        times = math.ceil(len(array_bits_convcoded) / len(P_m_bool))
+        # 扩展bool矩阵到数据长度
+        P_m_bool = numpy.tile(P_m_bool, times)[:len(array_bits_convcoded)]
+        # 打孔
+        puncture_result = array_bits_convcoded[P_m_bool]
+        array_bits_convcoded = puncture_result
+        print(array_bits_convcoded)
+
     # 将比特数组转换为字节数组
     array_bytes_convcoded = numpy.packbits(array_bits_convcoded)
     # 转换为字节
@@ -52,5 +72,6 @@ if __name__ == "__main__":
 
     # 寄存器个数，可以多写，不能少写
     m = 4
-    bytes_encoded = encode(ms_bytes, gen, m, g2reverse=True)
+    pun_matrix = [[1,1,0], [1,0,1]]
+    bytes_encoded = encode(ms_bytes, gen, m, g2reverse=True,puncture_matrix=pun_matrix)
     print(bytes_encoded)
